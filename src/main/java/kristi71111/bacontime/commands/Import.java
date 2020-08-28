f181ac326ee5b5c7697f2ee1a068e2ce2a291178
kristi71111 <kristjan.gorinek20@gmail.com>
2020-08-28 21:03:24 +0200

    Better import handling.

--- src/main/java/kristi71111/bacontime/commands/Import.java
+++ src/main/java/kristi71111/bacontime/commands/Import.java
@@ -1,11 +1,17 @@
 package kristi71111.bacontime.commands;
 
 import kristi71111.bacontime.definitions.ImportFromActiveTime;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import java.util.Optional;
 
 public class Import implements CommandExecutor {
     @Override
@@ -13,7 +19,12 @@ public class Import implements CommandExecutor {
         if (args.getOne("plugin").isPresent()) {
             String plugin = (String) args.getOne("plugin").get();
             if (plugin.equalsIgnoreCase("activetime")) {
-                ImportFromActiveTime.importFromActiveTime(src);
+                Optional<PluginContainer> pluginContainer = Sponge.getPluginManager().getPlugin("activetime");
+                if (pluginContainer.isPresent()) {
+                    ImportFromActiveTime.importFromActiveTime(src);
+                } else {
+                    throw new CommandException(Text.of(TextColors.RED, "ActiveTime is not present!"));
+                }
             }
         }
         return CommandResult.success();
--- src/main/java/kristi71111/bacontime/definitions/ImportFromActiveTime.java
+++ src/main/java/kristi71111/bacontime/definitions/ImportFromActiveTime.java
@@ -9,60 +9,52 @@ import kristi71111.bacontime.handlers.DataHandler;
 import kristi71111.bacontime.handlers.objects.BaconTimePlayerObject;
 import kristi71111.bacontime.handlers.objects.BaconTimeReachedMilestoneObject;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandSource;
-import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public class ImportFromActiveTime {
     public static void importFromActiveTime(CommandSource src) {
         Task.Builder taskBuilder = Task.builder();
-        Optional<PluginContainer> pluginContainer = Sponge.getPluginManager().getPlugin("activetime");
         taskBuilder.execute(() -> {
-                    if (pluginContainer.isPresent()) {
-                        try {
-                            Path storDir = ActiveTime.getInstance().getDirectory().resolve("configuration");
-                            ConfigHolder milestoness = new ConfigHolder(storDir.resolve("milestones.conf"), true);
-                            for (CommentedConfigurationNode m : milestoness.getNode().getChildrenMap().values()) {
-                                String name = (String) m.getKey();
-                                src.sendMessage(Text.of("Now importing milestone: " + name));
-                                ConfigHandler.config.getNode("milestones", name);
-                                ConfigHandler.config.getNode(new Object[]{"milestones", name, "requiredTime"}).setValue(m.getNode("activetime").getInt());
-                                ConfigHandler.config.getNode(new Object[]{"milestones", name, "commands"}).setValue(Helpers.getStringList(m.getNode("commands"), "milestone commands"));
-                                ConfigHandler.config.getNode(new Object[]{"milestones", name, "repeatable"}).setValue(m.getNode("repeatable").getBoolean(false));
-                            }
-                            ConfigHandler.save();
-                            ConfigHandler.loadValues();
-                            src.sendMessage(Text.of(TextColors.GREEN, "ActiveTime import of milestones complete!"));
-                            //Players
-                            storDir = ActiveTime.getInstance().getDirectory().resolve("storage");
-                            ConfigHolder players = new ConfigHolder(storDir.resolve("players.stor"), false);
-                            DataHandler.players.clear();
-                            for (CommentedConfigurationNode node : players.getNode().getChildrenMap().values()) {
-                                List<BaconTimeReachedMilestoneObject> milestones = new ObjectArrayList<>();
-                                for (CommentedConfigurationNode milestone : node.getNode("milestones").getChildrenMap().values()) {
-                                    if (ConfigHandler.AllMilestones.containsKey(milestone.getKey())) {
-                                        milestones.add(new BaconTimeReachedMilestoneObject((String) milestone.getKey(), (int) milestone.getValue(), ConfigHandler.AllMilestones.get(milestone.getKey()).isRepeatable()));
-                                    } else {
-                                        milestones.add(new BaconTimeReachedMilestoneObject((String) milestone.getKey(), (int) milestone.getValue(), false));
-                                    }
-                                }
-                                DataHandler.addPlayer(new BaconTimePlayerObject(node.getNode("activetime").getInt(), node.getNode("afktime").getInt(), node.getNode("username").getString(), UUID.fromString(node.getKey().toString()), milestones));
-                            }
-                            src.sendMessage(Text.of(TextColors.GREEN, "ActiveTime import of players complete!"));
-                        } catch (Exception e) {
-                            src.sendMessage(Text.of("Error check console"));
-                            e.printStackTrace();
+                    try {
+                        Path storDir = ActiveTime.getInstance().getDirectory().resolve("configuration");
+                        ConfigHolder milestoness = new ConfigHolder(storDir.resolve("milestones.conf"), true);
+                        for (CommentedConfigurationNode m : milestoness.getNode().getChildrenMap().values()) {
+                            String name = (String) m.getKey();
+                            src.sendMessage(Text.of("Now importing milestone: " + name));
+                            ConfigHandler.config.getNode("milestones", name);
+                            ConfigHandler.config.getNode(new Object[]{"milestones", name, "requiredTime"}).setValue(m.getNode("activetime").getInt());
+                            ConfigHandler.config.getNode(new Object[]{"milestones", name, "commands"}).setValue(Helpers.getStringList(m.getNode("commands"), "milestone commands"));
+                            ConfigHandler.config.getNode(new Object[]{"milestones", name, "repeatable"}).setValue(m.getNode("repeatable").getBoolean(false));
                         }
-                    } else {
-                        src.sendMessage(Text.of("ActiveTime is not present on the server!"));
+                        ConfigHandler.save();
+                        ConfigHandler.loadValues();
+                        src.sendMessage(Text.of(TextColors.GREEN, "ActiveTime import of milestones complete!"));
+                        //Players
+                        storDir = ActiveTime.getInstance().getDirectory().resolve("storage");
+                        ConfigHolder players = new ConfigHolder(storDir.resolve("players.stor"), false);
+                        DataHandler.players.clear();
+                        for (CommentedConfigurationNode node : players.getNode().getChildrenMap().values()) {
+                            List<BaconTimeReachedMilestoneObject> milestones = new ObjectArrayList<>();
+                            for (CommentedConfigurationNode milestone : node.getNode("milestones").getChildrenMap().values()) {
+                                if (ConfigHandler.AllMilestones.containsKey(milestone.getKey())) {
+                                    milestones.add(new BaconTimeReachedMilestoneObject((String) milestone.getKey(), (int) milestone.getValue(), ConfigHandler.AllMilestones.get(milestone.getKey()).isRepeatable()));
+                                } else {
+                                    milestones.add(new BaconTimeReachedMilestoneObject((String) milestone.getKey(), (int) milestone.getValue(), false));
+                                }
+                            }
+                            DataHandler.addPlayer(new BaconTimePlayerObject(node.getNode("activetime").getInt(), node.getNode("afktime").getInt(), node.getNode("username").getString(), UUID.fromString(node.getKey().toString()), milestones));
+                        }
+                        src.sendMessage(Text.of(TextColors.GREEN, "ActiveTime import of players complete!"));
+                    } catch (Exception e) {
+                        src.sendMessage(Text.of("Error check console"));
+                        e.printStackTrace();
                     }
                 }
         ).async().submit(BaconTime.getInstance());
